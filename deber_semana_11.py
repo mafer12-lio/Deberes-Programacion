4dbafd0c9bb40a012d403d79cc74017de42a1fdf
mafer12-lio <majo12koki@gmail.com>
2026-03-08 22:45:02 -0500

    semana 12

--- deber_semana_11.py
+++ deber_semana_11.py
@@ -1,1 +1,13 @@
-#crea todo el codigo
+asientos = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+            ]
+f = int(input("INGRESA FILA (0 a 2): "))
+c = int(input("INGRESA COLUMNA (0 a 3): "))
+
+asientos[f][c]  = 1
+
+for i in range(3):
+    for j in range(4):
+        print(asientos[i][j], end= " ")
+        print ()
